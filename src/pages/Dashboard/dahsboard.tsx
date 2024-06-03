e05698a23900eeae6eb31b9d1df6a99e0b850ddf
araujoedu7 <ea144027@gmail.com>
2024-06-03 14:55:29 -0300

    dashboard dinâmica

--- src/pages/Dashboard/dahsboard.tsx
+++ src/pages/Dashboard/dahsboard.tsx
@@ -1,17 +1,9 @@
-import { useContext } from "react";
+import { Outlet } from "react-router-dom";
 import Aside from "../../components/Aside/aside";
 import Header from "../../components/Header/header";
-import { AuthUser } from "../../context/authContext";
-import componente_img from "../../assets/estoque.png";
-import computador_img from "../../assets/computador_icon.png";
-import notebook_img from "../../assets/notebook_icon.png";
-import cabos_img from "../../assets/cabos_icon.png";
-import materiais_img from "../../assets/materiais_icon.png";
 
 
 export default function Dashboard() {
-
-    const { user } = useContext(AuthUser);
 
     return (
         <div className="flex h-screen overflow-hidden">
@@ -20,49 +12,7 @@ export default function Dashboard() {
             <div className="w-full">
                 <Header />
                 <div className="p-[10px] sm:p-[40px] bg-slate-100 h-customHeight overflow-auto">
-                    <div className="flex flex-col gap-5">
-                        <span className="font-semibold text-2xl">Bem-Vindo, {user?.name}</span>
-                        <span className="text-zinc-400 text-xl">Overview</span>
-                    </div>
-                    <div className="mt-[35px]">
-                        <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
-                            <div className="flex items-center w-full bg-white p-6 rounded-[20px] gap-5">
-                                <img src={componente_img} alt="componente_img" />
-                                <div className="flex flex-col">
-                                    <span className="text-greenAFS-100 text-xl">Componentes</span>
-                                    <span className="text-2xl font-semibold">10</span>
-                                </div>
-                            </div>
-                            <div className="flex items-center w-full bg-white p-5 rounded-[20px] gap-5">
-                                <img src={computador_img} alt="computador_img" />
-                                <div className="flex flex-col">
-                                    <span className="text-greenAFS-100 text-xl">Computador</span>
-                                    <span className="text-2xl font-semibold">8</span>
-                                </div>
-                            </div>
-                            <div className="flex items-center w-full bg-white p-5 rounded-[20px] gap-5">
-                                <img src={notebook_img} alt="notebook_img" />
-                                <div className="flex flex-col">
-                                    <span className="text-greenAFS-100 text-xl">Notebook</span>
-                                    <span className="text-2xl font-semibold">15</span>
-                                </div>
-                            </div>
-                            <div className="flex items-center w-full bg-white p-5 rounded-[20px] gap-5">
-                                <img src={materiais_img} alt="materiais_img" />
-                                <div className="flex flex-col">
-                                    <span className="text-greenAFS-100 text-xl">Materiais</span>
-                                    <span className="text-2xl font-semibold">7</span>
-                                </div>
-                            </div>
-                            <div className="flex items-center w-full bg-white p-5 rounded-[20px] gap-5">
-                                <img src={cabos_img} alt="cabos_img" />
-                                <div className="flex flex-col">
-                                    <span className="text-greenAFS-100 text-xl">Cabos</span>
-                                    <span className="text-2xl font-semibold">9</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Outlet />
                 </div>
             </div>
         </div>
